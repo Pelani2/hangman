779bf501f95be3b480bfd90a610d7905f5045d31
Igor Zupunski <ipatheword@live.com>
2023-08-24 23:23:21 +0200

    updates

--- src/components/HangmanGame/index.js
+++ src/components/HangmanGame/index.js
@@ -5,6 +5,7 @@ import WordDisplay from "../WordDisplay";
 
 const HangmanGame = () => {
     const dispatch = useDispatch();
+    const word = useSelector((state) => state.game.word);
     const incorrectGuesses = useSelector((state) => state.game.incorrectGuesses);
     const guessedLetters = useSelector((state) => state.game.guessedLetters);
 
@@ -17,29 +18,38 @@ const HangmanGame = () => {
         dispatch(selectRandomWord());
     };
 
+    const hasWon = word.split("").every((letter) => guessedLetters.includes(letter));
+    const hasLost = incorrectGuesses >= 6;
+
     return (
         <div>
             <h1>
                 Hangman
             </h1>
-            <WordDisplay />
-            <p>
-                Incorrect guesses: {incorrectGuesses}
-            </p>
-            <p>
-                Guessed letters: {guessedLetters.join(", ")}
-            </p>
-            <div>
-                {[...'abcdefghijklmnopqrstuvwxyz'].map((letter) => (
-                    <button
-                        key={letter}
-                        onClick={() => handleLetterClick(letter)}
-                        disabled={guessedLetters.includes(letter)}
-                    >
-                        {letter}
-                    </button>
-                ))}
-            </div>
+            {hasWon && <p> Congratulations, you won!</p>}
+            {hasLost && <p> Sorry, you lost. The word was: {word} </p>}
+            {!hasWon && !hasLost && (
+                <div>
+                    <WordDisplay />
+                    <p>
+                        Incorrect guesses: {incorrectGuesses}
+                    </p>
+                    <p>
+                        Guessed letters: {guessedLetters.join(", ")}
+                    </p>
+                    <div>
+                        {[...'abcdefghijklmnopqrstuvwxyz'].map((letter) => (
+                            <button
+                                key={letter}
+                                onClick={() => handleLetterClick(letter)}
+                                disabled={guessedLetters.includes(letter)}
+                            >
+                                {letter}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            )}
             <button onClick={handleResetClick}>
                 Reset
             </button>
