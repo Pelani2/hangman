65bbf6c3c50f2b9d973df7a13f95b89248644a3e
Igor Zupunski <ipatheword@live.com>
2023-08-25 17:25:24 +0200

    finished hint slice

--- src/redux/Reducers/hintSlice.js
+++ src/redux/Reducers/hintSlice.js
@@ -6,5 +6,28 @@ export const requestHint = createAsyncThunk(
     async (_, { getState }) => {
         const state = getState();
         const { word, maxGuesses, } = state.game;
+
+        const randomIndex = Math.floor(Math.random() * maxGuesses);
+        
+        return word[randomIndex];
     },  
 );
+
+const initialState = {
+    hint: "",
+};
+
+const hintSlice = createSlice({
+    name: "hint",
+    initialState,
+    extraReducers: {
+        [requestHint.pending]: (state) => {
+            state.hint = "";
+        },
+        [requestHint.fulfilled]: (state, action) => {
+            state.hint = action.payload;
+        },
+    },
+});
+
+export default hintSlice.reducer;
