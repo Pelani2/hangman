d81da46b3d9319a82f484a3544b5b1f4abaf9397
Igor Zupunski <ipatheword@live.com>
2023-08-26 11:47:13 +0200

    did timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -10,6 +10,31 @@ const Timer = ({ expiryTimestamp, onExpire }) => {
         resume,
         restart,
     } = useTimer({ expiryTimestamp, onExpire });
+
+    return (
+        <div>
+            <div>
+                <span>
+                    {minutes}
+                </span>
+                <span>
+                    {seconds}
+                </span>
+            </div>
+            <button onClick={start}>
+                Start
+            </button>
+            <button onClick={pause}>
+                Pause
+            </button>
+            <button onClick={resume}>
+                Resume
+            </button>
+            <button onClick={() => restart(expiryTimestamp)}>
+                Restart
+            </button>
+        </div>
+    )
 };
 
 export default Timer;
